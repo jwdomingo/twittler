27b4de0f0e29a10e359f649922aef5c5bcd4e32e
John Domingo <johnwill.domingo@gmail.com>
2015-12-01 23:26:32 -0800

    Modify frequency of new tweets

--- data_generator.js
+++ data_generator.js
@@ -53,7 +53,7 @@ for(var i = 0; i < 10; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 5000);
+  setTimeout(scheduleNextTweet, Math.random() * 10000);
 };
 scheduleNextTweet();
 
